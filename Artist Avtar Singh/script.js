9d5c33d52729c447583ba2ce17eb06aa93cfb5a3
Avtar <officialavtar13@gmail.com>
2025-01-16 00:35:51 +0530

    update artist avtar singh

--- Artist Avtar Singh/script.js
+++ Artist Avtar Singh/script.js
@@ -1,3 +1,5 @@
+
+
 document.querySelector('.nav-toggle').addEventListener('click', () => {
     const navMenu = document.querySelector('.nav-menu');
     navMenu.classList.toggle('active');
@@ -5,8 +7,43 @@ document.querySelector('.nav-toggle').addEventListener('click', () => {
 
 
 function disableRightClick(event) {
-    if (event.button == 2) {
+    if (event.button === 2) {
       event.preventDefault(); // Prevent the default right-click behavior
       return false;
     }
 }
+
+(function(){
+  emailjs.init('gz5S6KhNqlW8vHpvu');
+})();
+
+document.querySelector('.submit-btn').addEventListener('click',(event)=>{
+  event.preventDefault(); 
+
+  const userName = document.querySelector('#name').value;
+  const userEmail = document.querySelector('#email').value;
+  const canvaSize = document.querySelector('input[name="size"]:checked')?.value || 'Not selected';
+  const medium = document.querySelector('input[name="medium"]:checked')?.value || 'Not selected';
+  const message = document.querySelector('#message').value;
+  
+  const formData = {
+      name: userName,
+      email: userEmail,
+      size: canvaSize,
+      medium: medium,
+      message: message,
+  };
+  
+
+  emailjs.send('service_mk95j72', 'template_9gg7izp', formData).then(
+    (response) => {
+      console.log('SUCCESS!', response.status, response.text);
+      alert('Message sent successfully!');
+    },
+    (error) => {
+      console.log('FAILED...', error);
+      alert('Failed to send the message. Please try again.');
+    },
+  );
+
+})
